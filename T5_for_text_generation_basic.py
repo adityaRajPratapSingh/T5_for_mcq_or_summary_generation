165fa89d3c27f37965555d5ef8e6ce8ec20adb15
Aditya <155698542+adityaRajPratapSingh@users.noreply.github.com>
2024-10-26 03:01:06 +0530

    empty notebook

---
+++ T5_for_text_generation_basic.py
@@ -0,0 +1,39 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOl+wIzE1hiIKcqqqtRLUPy",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/adityaRajPratapSingh/T5_for_text_generation_basic/blob/main/T5_for_text_generation_basic.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "D1Ae12NRFS8w"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
